287015cd431836f7a9c9f9e5c3f2d1c80d2994fb
rbokel <rbokel.github@qlod.org>
2015-06-12 16:06:52 +0200

    Update example app with new version

--- example/project/Build.scala
+++ example/project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
   val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "org.corespring" %% "s3-play-plugin" % "0.9-SNAPSHOT"
+    "org.corespring" %% "s3-play-plugin" % "1.1.0-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
